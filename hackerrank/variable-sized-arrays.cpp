de4a12b5a40f63ab4860edf107599a35c46865bd
leejonghyuen <morningmem@hanyang.ac.kr>
2016-10-07 21:26:50 +0900

    Update variable-sized-arrays.cpp
    Implementation without the vector in accordance with the intentions of the problem

--- hackerrank/variable-sized-arrays.cpp
+++ hackerrank/variable-sized-arrays.cpp
@@ -1,26 +1,28 @@
-//template is funny
-int main(){
-	void f();
-	f();
-}
-
-#include <vector>
-#include <cstdio>
+#include <iostream>
 using namespace std;
 
-void f(){
-	int N,Q;
-	scanf("%d%d",&N,&Q);
-	vector<vector<int> >v(N);
-	for(int i=0;i<N;i++){
+int main() {
+	int N, Q;
+
+	scanf("%d %d", &N, &Q);
+
+	int** va = (int **)alloca(sizeof(int*) * N);
+
+	for (int i = 0; i<N; i++) {
 		int M;
-		scanf("%d",&M);
-		v[i].resize(M);
-		for(int j=0;j<M;j++)scanf("%d",&v[i][j]);
+		scanf("%d", &M);
+
+    	int* subVa = (int *)alloca(sizeof(int) * M);
+		for (int j = 0; j<M; j++)
+			scanf("%d", &subVa[j]);
+		va[i] = subVa;
 	}
-	for(int i=0;i<Q;i++){
-		int a,b;
-		scanf("%d%d",&a,&b);
-		printf("%d\n",v[a][b]);
+
+	for (int i = 0; i<Q; i++) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+		printf("%d\n", va[a][b]);
 	}
+
+	return 0;
 }
